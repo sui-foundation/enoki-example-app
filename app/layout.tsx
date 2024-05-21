fd521d825901e484834b8cccf7e64b8c26672a7d
Daniel Leavitt <daniel.7039@hotmail.com>
2024-05-21 11:25:44 -0700

    example transaction works!

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,16 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { EnokiFlowProvider } from "@mysten/enoki/react";
+import { createNetworkConfig, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui.js/client';
 
 const inter = Inter({ subsets: ["latin"] });
+
+ 
+// Config options for the networks you want to connect to
+const { networkConfig } = createNetworkConfig({
+	testnet: { url: getFullnodeUrl('testnet') },
+});
 
 export default function RootLayout({
   children,
@@ -14,9 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <EnokiFlowProvider apiKey="enoki_public_818d52256623b170c33438c51070eaa0">
-        <body className={inter.className}>{children}</body>
-      </EnokiFlowProvider>
+      <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <EnokiFlowProvider apiKey="enoki_public_818d52256623b170c33438c51070eaa0">
+          <body className={inter.className}>{children}</body>
+        </EnokiFlowProvider>
+      </SuiClientProvider>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import { useEnokiFlow } from "@mysten/enoki/react";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { jwtDecode } from 'jwt-decode';
+import { TransactionBlock } from '@mysten/sui.js/transactions';
+import { useSuiClient } from '@mysten/dapp-kit';
 
 export default function Home() {
 
+  const client = useSuiClient();
   const enokiFlow = useEnokiFlow();
   const [session, setSession] = useState<any | null>(null);
-  const [keypair, setKeypair] = useState<any | null>(null);
- 
   
   useEffect(() => {
     completeLogin();
@@ -38,27 +39,44 @@ export default function Home() {
         const session = await enokiFlow.getSession();
         console.log('session', session);
         setSession(session);
-
-        const keypair = await enokiFlow.getKeypair({network: 'testnet'});
-        console.log('keypair', keypair);
-        setKeypair(keypair);
       } catch (error) {
         console.error('error', error);
       }
 
       // remove the URL fragment
       window.history.replaceState(null, '', window.location.pathname);
-    }
-
-    
+    } 
   }
+
+  async function handleButtonClick() {
+		// Get the keypair for the current user.
+		const keypair = await enokiFlow.getKeypair();
+
+    const address = keypair.toSuiAddress();
+    console.log('address', address);
+
+		const txb = new TransactionBlock();
+    
+		// Add some transactions to the block...
+    const [coin] = txb.splitCoins(txb.gas, [txb.pure(1)]);
+    txb.transferObjects([coin], txb.pure('0x8e8cae7791a93778800b88b6a274de5c32a86484593568d38619c7ea71999654'));
+
+		// Sign and execute the transaction block, using the Enoki keypair
+		const res = await client.signAndExecuteTransactionBlock({
+			signer: keypair,
+			transactionBlock: txb,
+		});
+    console.log('res', res)
+	}
 
   if (session) {
     return (
       <div className="flex flex-col items-center justify-start">
         <h1>Welcome!</h1>
         <p className="max-w-md">Session: {JSON.stringify(session, null, 4)}</p>
-        <p>Keypair: {JSON.stringify(keypair, null, 4)}</p>
+        <button onClick={handleButtonClick}>
+          Sign transaction
+        </button>
         <button onClick={async () => {
           await enokiFlow.logout();
           window.location.reload();
