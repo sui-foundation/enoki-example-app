97cf74cdaa9a6bef9f35308eaa919ab19aa4d7b7
Daniel Leavitt <daniel.7039@hotmail.com>
2024-10-10 11:54:05 -0700

    cleaning

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Transaction } from "@mysten/sui/transactions";
-import { useSuiClient } from "@mysten/dapp-kit";
-import { useEnokiFlow, useZkLogin } from "@mysten/enoki/react";
-import { getFaucetHost, requestSuiFromFaucetV0 } from "@mysten/sui/faucet";
-import { ExternalLink, Github, LoaderCircle, RefreshCw } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { toast } from "sonner";
-import { BalanceChange } from "@mysten/sui/client";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { track } from "@vercel/analytics";
 import { useCustomWallet } from "@/contexts/CustomWallet";
 import ProfilePopover from "@/components/ProfilePopover";
 import { CreateCounter } from "@/components/CreateCounter";
@@ -33,13 +8,15 @@ import { isValidSuiObjectId } from "@mysten/sui/utils";
 import { Counter } from "@/components/Counter";
 
 export default function Page() {
-
   const { isConnected } = useCustomWallet();
-  const [counterId, setCounter] = useState(() => {
-  	const hash = window.location.hash.slice(1);
-  	return isValidSuiObjectId(hash) ? hash : null;
-  });
+  const [counterId, setCounter] = useState<string | null>(null);
 
+  useEffect(() => {
+    const hash = window.location.hash.slice(1);
+    if (isValidSuiObjectId(hash)) {
+      setCounter(hash);
+    }
+  }, []);
 
   return (
     <div className="w-full h-full min-h-screen p-2">
@@ -48,24 +25,20 @@ export default function Page() {
         <ProfilePopover />
       </div>
       <div className="flex flex-col items-center sm:flex-row gap-4 sm:items-start">
-        {
-          isConnected ? (
-            counterId ? (
-  						<Counter id={counterId} />
-  					) : (
-  						<CreateCounter
-  							onCreated={(id) => {
-  								window.location.hash = id;
-  								setCounter(id);
-  							}}
-  						/>
-  					)
+        {isConnected ? (
+          counterId ? (
+            <Counter id={counterId} />
           ) : (
-            <div>
-              Please connect your wallet to continue.
-            </div>
+            <CreateCounter
+              onCreated={(id) => {
+                window.location.hash = id;
+                setCounter(id);
+              }}
+            />
           )
-        }
+        ) : (
+          <div>Please connect your wallet to continue.</div>
+        )}
       </div>
     </div>
   );
--- helpers/formatString.ts
+++
@@ -1,6 +0,0 @@
-export const formatString = (str: string, length: number) => {
-  if (str.length <= length) {
-    return str;
-  }
-  return str.slice(0, length) + "...";
-};
